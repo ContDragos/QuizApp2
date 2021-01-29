9278534a08820d47a8cc8ca495df9d7af867abd3
Dragos Cont <dragoscont@live.com>
2021-01-29 18:37:54 +0200

    added volley library dependency

--- app/src/main/java/com/example/quizapp2/MainActivity.java
+++ app/src/main/java/com/example/quizapp2/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.quizapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue mRequestQueue;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRequestQueue = VolleySingleton.getInstance().getRequestQueue();
+        url = "https://opentdb.com/api.php?amount=10&category=11&type=boolean";
+
+        JsonObjectRequest filmsJsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("TAG", response + "");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        mRequestQueue.add(filmsJsonObjectRequest);
     }
 }
